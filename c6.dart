df46ae2812998d34d0cbb57989c41dfdc5b46a5e
Bnttrang2002 <125441880+Bnttrang2002@users.noreply.github.com>
2023-02-28 23:37:15 +0700

    Update c6.dart

--- c6.dart
+++ c6.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 void main(){
-  stdout.write('Mời bạn nhập ho : ');
+  stdout.write('Mời bạn nhập họ : ');
   String firstName = stdin.readLineSync()!;
- stdout.write('Mời bạn nhập ten : ');
+ stdout.write('Mời bạn nhập tên : ');
   String lastName = stdin.readLineSync()!;
   print("Full name is $firstName $lastName");
 }
